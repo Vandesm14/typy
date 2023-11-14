be22437bfc40e31c804cd3338cfd7162509b7a4c
Shane Vandegrift <vandesm14@gmail.com>
2023-11-14 14:25:35 -0500

    mvp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,66 @@
 <script lang="ts">
   import '../style.css';
 
-  let count: number = 0;
+  const sleep = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
 
-  function handleClick() {
-    count += 1;
+  let text = 'Greetings, Traveler. Your mission is to decrypt this message.';
+  let state: 'streaming' | 'decrypting' | 'done' = 'done';
+  let liveText: { char: string; font: string }[] = [];
+  let fonts = ['argon', 'krypton', 'neon', 'radon', 'xenon'];
+
+  function randomChar(): string {
+    const chars =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    return chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  function randomFont() {
+    return fonts[Math.floor(Math.random() * fonts.length)];
+  }
+
+  async function startStreaming() {
+    state = 'streaming';
+    liveText = [];
+
+    for (let i in text.split('')) {
+      if (text[i] === ' ') {
+        liveText = [...liveText, { char: ' ', font: '' }];
+      } else {
+        liveText = [...liveText, { char: randomChar(), font: randomFont() }];
+      }
+
+      await sleep(40);
+    }
+
+    state = 'decrypting';
+    startDecrypting();
+  }
+
+  async function startDecrypting() {
+    for (let index in liveText) {
+      liveText[index] = { char: text[index], font: randomFont() };
+      await sleep(60);
+    }
+
+    state = 'done';
   }
 </script>
 
-<h1>Hello world!</h1>
-<button on:click={handleClick}>Clicked {count} times</button>
+<main>
+  <p>
+    {#each liveText as { char, font }, i (i)}
+      {#if char === ' '}
+        <span>&nbsp;</span>
+      {:else}
+        <span class={font}>{char}</span>
+      {/if}
+    {/each}
+  </p>
+  <ul>
+    <li>{state}</li>
+  </ul>
+  <button on:click={startStreaming} disabled={state === 'streaming'}
+    >Start</button
+  >
+</main>

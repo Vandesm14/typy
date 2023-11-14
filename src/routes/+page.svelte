41a47a4f3b5b95eae3dc6e07884c94ea3580d439
Shane Vandegrift <vandesm14@gmail.com>
2023-11-14 14:07:34 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import '../style.css';
+
+  let count: number = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<h1>Hello world!</h1>
+<button on:click={handleClick}>Clicked {count} times</button>
